feb0ff6f133d1533e1fd141326804b3fbc53f333
AhmedAbdeen7 <127099805+AhmedAbdeen7@users.noreply.github.com>
2024-06-30 17:53:45 +0300

    Fixed the errors in the reading of the S types

--- rvsim versions/rvsim.cpp
+++ rvsim versions/rvsim.cpp
@@ -21,6 +21,7 @@ string abiName[32] = {"zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2", "s0", "s
                       "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7", "s2", "s3", "s4", "s5",
                       "s6", "s7", "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
 unsigned char memory[(64 + 64) * 1024];
+void instDecExec(unsigned int instWord);
 
 void emitError(char *s)
 {
@@ -31,6 +32,66 @@ void emitError(char *s)
 void printPrefix(unsigned int instA, unsigned int instW)
 {
     cout << "0x" << hex << std::setfill('0') << std::setw(8) << instA << "\t0x" << std::setw(8) << instW;
+}
+bool isCompressed(unsigned int instWord)
+{
+    unsigned int opcode = instWord & 0x00000003;
+    if (opcode == 0x3)
+    {
+        return false;
+    }
+    return true;
+}
+unsigned int deCompress(unsigned int instWord)
+{
+    // if (!isCompressed(instWord))
+    // {
+    // 	return;
+    // }
+    instWord = 0x00000114;
+    unsigned int rd, rs1, rs2, funct4, funct3, offset, op;
+    unsigned int CIW_imm, CL_imm;
+
+    op = instWord & 0x00000003;
+    funct3 = (instWord >> 13) & 0x00000007;
+    funct4 = (instWord >> 12) & 0x0000000F;
+    offset = (instWord >> 2) & 0x000007FF;
+    if (op == 0x0)
+    { // CL-CS-CIW Formats
+        switch (funct3)
+        {
+        case 0:
+            // CIW - Format
+            rd = (instWord >> 2) & 0x00000007;
+            CIW_imm = (instWord >> 5) & 0x000000FF;
+            instWord = 0x00000013; // I-type, This is not entirely correct, To be Solved Later:/
+            rd = rd << 7;
+            instWord |= rd;
+            instWord |= 0x00000000;  // ADDI
+            instWord |= 0x00010000;  // sp ~x2
+            CIW_imm = CIW_imm << 2;  // 4*imm
+            CIW_imm = CIW_imm << 20; // place imm[0] at bit 20
+            instWord |= CIW_imm;
+            instDecExec(instWord);
+
+            break;
+        case 2:
+            // CL - Format
+            rd = (instWord >> 2) & 0x00000007;
+            instWord = 0x00000003; // I-Type LOAD
+            rd = rd << 7;
+            instWord |= rd;
+            // Rest TBC
+
+            break;
+        case 6:
+            break;
+
+        default:
+            break;
+        }
+    }
+    return instWord;
 }
 
 void instDecExec(unsigned int instWord)
@@ -50,8 +111,15 @@ void instDecExec(unsigned int instWord)
     shamt = (instWord >> 20) & 0x0000001F;
     // — inst[31] — inst[30:25] inst[24:21] inst[20]
     I_imm = ((instWord >> 20) & 0x7FF) | (((instWord >> 31) ? 0xFFFFF800 : 0x0));
-    S_imm = ((instWord >> 7) & 0x1F) | ((instWord >> 20) & 0xFFF) | ((instWord >> 31) ? 0xFFFFF800 : 0x0);
-    // inst
+    S_imm = ((instWord >> 7) & 0x1F) | ((instWord >> 20) & 0x3E0) | (((instWord >> 31) ? 0xFFFFF800 : 0x0));
+    // int imm4_0 = (instWord >> 7) & 0x1F;
+
+    // // Extract the upper 7 bits (bits 25-31)
+    // int imm11_5 = (instWord >> 25) & 0x7F;
+
+    // // Combine the two parts and sign-extend
+    // S_imm = (imm11_5 << 5) | imm4_0;
+    // // inst
 
     printPrefix(instPC, instWord);
 
@@ -81,11 +149,11 @@ void instDecExec(unsigned int instWord)
             break;
         case 2:
             cout << "\tSLT\t" << abiName[rd] << ", " << abiName[rs1] << ", " << abiName[rs2] << "\n";
-            reg[rd] = (reg[rs1] < reg[rs2]) ? 1 : 0;
+            reg[rd] = (int(reg[rs1]) < int(reg[rs2])) ? 1 : 0;
             break;
         case 3:
             cout << "\tSLTU\t" << abiName[rd] << ", " << abiName[rs1] << ", " << abiName[rs2] << "\n";
-            reg[rd] = (static_cast<unsigned int>(reg[rs1]) < static_cast<unsigned int>(reg[rs2])) ? 1 : 0;
+            reg[rd] = (reg[rs1] < reg[rs2]) ? 1 : 0;
             break;
         case 4:
             cout << "\tXOR\t" << abiName[rd] << ", " << abiName[rs1] << ", " << abiName[rs2] << "\n";
@@ -133,11 +201,28 @@ void instDecExec(unsigned int instWord)
             break;
         case 2:
             cout << "\tSLTI\t" << abiName[rd] << ", " << abiName[rs1] << ", " << dec << (int)I_imm << "\n";
-            reg[rd] = (reg[rs1] < int(I_imm)) ? 1 : 0;
+            if (int(reg[rs1]) < int(I_imm))
+            {
+                reg[rd] = 1;
+            }
+            else
+            {
+                reg[rd] = 0;
+            }
+
+            // reg[rd] = (reg[rs1] < reg[rs2]) ? 1 : 0;
             break;
         case 3:
             cout << "\tSLTIU\t" << abiName[rd] << ", " << abiName[rs1] << ", " << dec << (int)I_imm << "\n";
-            reg[rd] = (static_cast<unsigned int>(reg[rs1]) < static_cast<unsigned int>(I_imm)) ? 1 : 0;
+            if (static_cast<unsigned int>(reg[rs1]) < static_cast<unsigned int>(I_imm))
+            {
+                reg[rd] = 1;
+            }
+            else
+            {
+                reg[rd] = 0;
+            }
+            // reg[rd] = (static_cast<unsigned int>(reg[rs1]) < static_cast<unsigned int>(I_imm)) ? 1 : 0;
             break;
         case 4:
             cout << "\tXORI\t" << abiName[rd] << ", " << abiName[rs1] << ", " << dec << (int)I_imm << "\n";
@@ -210,26 +295,30 @@ void instDecExec(unsigned int instWord)
             cout << "\tUnkown I-type load Instruction \n";
         }
     }
-
-    if (opcode == 0x23)
+    else if (opcode == 0x23)
     {
         switch (funct3)
         {
         case 0:
-            cout << "\tSB\t" << abiName[rs1] << ", " << dec << (int)S_imm << "(" << abiName[rs2] << ")" << "\n";
+            cout << "\tSB\t" << abiName[rs2] << ", " << dec << (int)S_imm << "(" << abiName[rs1] << ")" << "\n";
             memory[rs1] = reg[rs2 + (int)S_imm];
+            break;
 
         case 1:
-            cout << "\tSh\t" << abiName[rs1] << ", " << dec << (int)S_imm << "(" << abiName[rs2] << ")" << "\n";
+            cout << "\tSh\t" << abiName[rs2] << ", " << dec << (int)S_imm << "(" << abiName[rs1] << ")" << "\n";
             memory[rs1] = reg[rs2 + (int)S_imm];
             memory[rs1] |= (reg[rs2 + (int)S_imm + 1] << 8);
+            break;
 
         case 2:
-            cout << "\tSW\t" << abiName[rs1] << ", " << dec << (int)S_imm << "(" << abiName[rs2] << ")" << "\n";
+            cout << "\tSW\t" << abiName[rs2] << ", " << dec << (int)S_imm << "(" << abiName[rs1] << ")" << "\n";
             memory[rs1] = reg[rs2 + (int)S_imm];
             memory[rs1] |= (reg[rs2 + (int)S_imm + 1] << 8);
             memory[rs1] |= (reg[rs2 + (int)S_imm + 1] << 16);
             memory[rs1] |= (reg[rs2 + (int)S_imm + 1] << 24);
+            break;
+        default:
+            cout << "\tUnkown I-type load Instruction \n";
         }
     }
     else
@@ -237,29 +326,52 @@ void instDecExec(unsigned int instWord)
         cout << "\tUnkown Instruction \n";
     }
 }
+void printRegisterContents()
+{
+    for (int i = 0; i < 32; i++)
+    {
+        cout << "x" << dec << i << "\t" << abiName[i] << "\t0x" << hex << reg[i] << "\n";
+    }
+}
 
 int main(int argc, char *argv[])
 {
-
     unsigned int instWord = 0;
     ifstream inFile;
+    ifstream dataFile;
     ofstream outFile;
 
-    if (argc < 1)
-        emitError("use: rvcdiss <machine_code_file_name>\n");
+    if (argc < 2)
+        emitError("use: rvcdiss <machine_code_file_name> <data_section_file_name>\n");
 
     inFile.open(argv[1], ios::in | ios::binary | ios::ate);
-
+    dataFile.open(argv[2], ios::in | ios::binary | ios::ate);
     if (inFile.is_open())
     {
         int fsize = inFile.tellg();
 
         inFile.seekg(0, inFile.beg);
         if (!inFile.read((char *)memory, fsize))
-            emitError("Cannot read from input file\n");
+            emitError("Cannot read from text file\n");
+    }
+
+    if (dataFile.is_open())
+    {
+        int fsize = dataFile.tellg();
+
+        dataFile.seekg(0, dataFile.beg);
+        if (!dataFile.read((char *)(memory + 0x00010000), fsize)) // because data section starts at 0x00010000
+            emitError("Cannot read from data file\n");
+    }
+    else
+        emitError("Cannot access data file\n");
+
+    if (inFile.is_open())
+    {
 
         while (true)
         {
+            // deCompress(instWord);
             instWord = (unsigned char)memory[pc] |
                        (((unsigned char)memory[pc + 1]) << 8) |
                        (((unsigned char)memory[pc + 2]) << 16) |
@@ -268,9 +380,11 @@ int main(int argc, char *argv[])
             // remove the following line once you have a complete simulator
             if ((pc > 16000) | (instWord == 0))
                 break; // stop when PC reached address 32
+
             instDecExec(instWord);
         }
+        printRegisterContents();
     }
     else
-        emitError("Cannot access input file\n");
+        emitError("Cannot access text file\n");
 }
