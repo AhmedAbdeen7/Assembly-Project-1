5c95b37c985a1ce4a3f50de4ca9502579e7839ce
Aly Elaswad <124714695+alyelaswad@users.noreply.github.com>
2024-07-05 03:31:15 +0300

    Fixed J_imm, S_imm, JAL minor bug and updated L and S
    working on t1.bin!!

--- rvsim.cpp
+++ rvsim.cpp
@@ -17,11 +17,11 @@
 using namespace std;
 
 unsigned int pc;
-unsigned int reg[32] = {0};
+unsigned int reg[32];
 string abiName[32] = {"zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2", "s0", "s1",
 					  "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7", "s2", "s3", "s4", "s5",
 					  "s6", "s7", "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
-unsigned char memory[(64 + 64) * 1024] = {0};
+unsigned char memory[(64 + 64) * 1024];
 void instDecExec(unsigned int instWord);
 
 void emitError(char *s)
@@ -199,14 +199,21 @@ void instDecExec(unsigned int instWord)
 	shamt = (instWord >> 20) & 0x0000001F;
 	// — inst[31] — inst[30:25] inst[24:21] inst[20]
 	I_imm = ((instWord >> 20) & 0x7FF) | (((instWord >> 31) ? 0xFFFFF800 : 0x0));
-	S_imm = ((instWord >> 7) & 0x1F) | ((instWord >> 20) & 0x3E0) | (((instWord >> 31) ? 0xFFFFF800 : 0x0));
-	U_imm = ((instWord >> 12));
+	S_imm = ((instWord >> 7) & 0x1F) |	 // imm[4:0]
+			((instWord >> 20) & 0xFE0) | // imm[11:5]
+			((instWord & 0x80000000) ? 0xFFFFF800 : 0);
+	U_imm = ((instWord >> 12) & 0xFFFFF);
+	B_imm = (((instWord >> 8) & 0xF) << 1) | (((instWord >> 25) & 0x3F) << 5) | (((instWord >> 7) & 0x1) << 11) | (((instWord >> 31) & 0x1) << 12);
 	if (B_imm >> 12)
 		B_imm |= 0xFFFFF000;
-	J_imm = (((instWord >> 21) & 0x3FF) << 1) | (((instWord >> 12) & 0xFF) << 11) | ((instWord >> 31) << 20) | ((instWord >> 20) << 11);
-	if (J_imm >> 20)
-		J_imm |= 0xFFF00000;
-	;
+	J_imm = (((instWord >> 31) & 0x1) << 20) |
+			(((instWord >> 12) & 0xFF) << 12) |
+			(((instWord >> 20) & 0x1) << 11) |
+			(((instWord >> 21) & 0x3FF) << 1) |
+			(((instWord >> 31) & 0x1) ? 0xFFF00000 : 0);
+
+	// if (J_imm >> 20)
+	// 	J_imm |= 0xFFF00000;
 	// inst
 
 	printPrefix(instPC, instWord);
@@ -354,29 +361,37 @@ void instDecExec(unsigned int instWord)
 		{
 		case 0:
 			cout << "\tLB\t" << abiName[rd] << ", " << dec << (int)I_imm << "(" << abiName[rs1] << ")" << "\n";
-			reg[rd] = memory[rs1 + (int)I_imm];
+			reg[rd] = memory[reg[rs1] + (int)I_imm];
+			if (reg[rd] & 0x80)
+			{
+				reg[rd] |= 0xFFFFFF00; // If the MSB is set, extend the sign bit
+			}
 			break;
 		case 1:
 			cout << "\tLH\t" << abiName[rd] << ", " << dec << (int)I_imm << "(" << abiName[rs1] << ")" << "\n";
-			reg[rd] = memory[rs1 + (int)I_imm];
-			reg[rd] |= (memory[rs1 + (int)I_imm + 1] << 8);
+			reg[rd] = memory[reg[rs1] + (int)I_imm];
+			reg[rd] |= (memory[reg[rs1] + (int)I_imm + 1] << 8);
+			if (reg[rd] & 0x8000)
+			{
+				reg[rd] |= 0xFFFF0000; // If the MSB is set, extend the sign bit
+			}
 			break;
 		case 2:
 			cout << "\tLW\t" << abiName[rd] << ", " << dec << (int)I_imm << "(" << abiName[rs1] << ")" << "\n";
-			reg[rd] = memory[rs1 + (int)I_imm];
-			reg[rd] = (memory[rs1 + (int)I_imm + 1] << 8) & 0xFF;
-			reg[rd] = (memory[rs1 + (int)I_imm + 2] << 16) & 0xFF;
-			reg[rd] = (memory[rs1 + (int)I_imm + 3] << 24) & 0xFF;
+			reg[rd] = memory[reg[rs1] + (int)I_imm];
+			reg[rd] |= (memory[reg[rs1] + (int)I_imm + 1] << 8);
+			reg[rd] |= (memory[reg[rs1] + (int)I_imm + 2] << 16);
+			reg[rd] |= (memory[reg[rs1] + (int)I_imm + 3] << 24);
 			break;
 		case 4:
 			cout << "\tLBU\t" << abiName[rd] << ", " << dec << (int)I_imm << "(" << abiName[rs1] << ")" << "\n";
-			reg[rd] = static_cast<unsigned int>(memory[rs1 + (int)I_imm]);
+			reg[rd] = static_cast<unsigned int>(memory[reg[rs1] + (int)I_imm]);
 			reg[rd] &= 0x000000FF;
 			break;
 		case 5:
 			cout << "\tLHU\t" << abiName[rd] << ", " << dec << (int)I_imm << "(" << abiName[rs1] << ")" << "\n";
-			reg[rd] = static_cast<unsigned int>(memory[rs1 + (int)I_imm]) |
-					  (static_cast<unsigned int>(memory[rs1 + (int)I_imm + 1]) << 8);
+			reg[rd] = static_cast<unsigned int>(memory[reg[rs1] + (int)I_imm]) |
+					  (static_cast<unsigned int>(memory[reg[rs1] + (int)I_imm + 1]) << 8);
 			reg[rd] &= 0x0000FFFF;
 			break;
 		default:
@@ -387,12 +402,12 @@ void instDecExec(unsigned int instWord)
 	{ // JALR (I - Instruction)
 		cout << "\tJALR\t" << abiName[rd] << ", " << abiName[rs1] << ", " << dec << (int)I_imm << "\n";
 		reg[rd] = instPC + 4;
-		pc = rs1 + (int)I_imm;
+		pc = reg[rs1] + (int)I_imm;
 	}
 	else if (opcode == 0x73)
 
 	{ // ecall (I -Instruction)
-		cout << "\tECALL\t";
+		cout << "\tECALL\n";
 		if (reg[17] == 1)
 		{
 			cout << reg[10];
@@ -419,22 +434,23 @@ void instDecExec(unsigned int instWord)
 		{
 		case 0:
 			cout << "\tSB\t" << abiName[rs2] << ", " << dec << (int)S_imm << "(" << abiName[rs1] << ")" << "\n";
-			memory[rs1] = (unsigned int)reg[rs2 + (int)S_imm];
+			memory[reg[rs1] + (int)S_imm] = reg[rs2] & 0xFF;
 			break;
 
 		case 1:
 			cout << "\tSH\t" << abiName[rs2] << ", " << dec << (int)S_imm << "(" << abiName[rs1] << ")" << "\n";
-			memory[rs1] = (unsigned int)reg[rs2 + (int)S_imm];
-			memory[rs1] |= (unsigned int)(reg[rs2 + (int)S_imm] << 8);
+			memory[reg[rs1] + (int)S_imm] = reg[rs2] & 0xFF;
+			memory[reg[rs1] + (int)S_imm + 1] = (reg[rs2] >> 8) & 0xFF;
 			break;
 
 		case 2:
 			cout << "\tSW\t" << abiName[rs2] << ", " << dec << (int)S_imm << "(" << abiName[rs1] << ")" << "\n";
-			memory[reg[rs1] + (int)S_imm] = reg[rs2];
-			memory[reg[rs1] + (int)S_imm + 1] = ((reg[rs2] << 8) & 0xFF);
-			memory[reg[rs1] + (int)S_imm + 2] = ((reg[rs2] << 16) & 0xFF);
-			memory[reg[rs1] + (int)S_imm + 3] = ((reg[rs2] << 24) & 0xFF);
+			memory[reg[rs1] + (int)S_imm] = reg[rs2] & 0xFF;
+			memory[reg[rs1] + (int)S_imm + 1] = (reg[rs2] >> 8) & 0xFF;
+			memory[reg[rs1] + (int)S_imm + 2] = (reg[rs2] >> 16) & 0xFF;
+			memory[reg[rs1] + (int)S_imm + 3] = (reg[rs2] >> 24) & 0xFF;
 			break;
+
 		default:
 			cout << "\tUnkown I-type load Instruction \n";
 		}
@@ -493,8 +509,7 @@ void instDecExec(unsigned int instWord)
 	}
 	else if (opcode == 0x6F)
 	{
-		cout << "\tJAL\t" << abiName[rd] << ", " << J_imm << "\n"
-			 << endl;
+		cout << "\tJAL\t" << abiName[rd] << ", " << dec << (int)J_imm << "\n";
 		reg[rd] = instPC + 4;
 		pc = instPC + J_imm;
 	}
